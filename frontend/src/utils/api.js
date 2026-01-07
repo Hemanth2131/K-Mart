95110254a027c22898b37a080f56e2ffb271867e
Hemanth2131 <mhemanth575@gmail.com>
2026-01-07 23:19:15 +0530

    update api base url for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://k-mart-gnma.onrender.com',
 });
 
 // Attach token before every request
